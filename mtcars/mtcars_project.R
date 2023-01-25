e2d84765506e536fb0873bcca82b6cf112491722
saeedf92 <farajzadeh92@gmail.com>
2023-01-25 13:39:47 -0500

    ggsave() added to save the figure iwth directory

--- mtcars/mtcars_project.R
+++ mtcars/mtcars_project.R
@@ -14,7 +14,6 @@ ggplot(mtcars, aes(cyl, mpg)) +
         axis.title.y = element_text(color="black", size=12, face="bold")) +
   xlab("Number of cylinders") +
   ylab("Miles per gallon") +
-  ggtitle("Association of number of cylinders and miles per gallon") +
-  ggsave()
+  ggtitle("Association of number of cylinders and miles per gallon")
   
 
