916ec81bf31286d50bb72b3308db17c6d9fd86e9
saeedf92 <farajzadeh92@gmail.com>
2023-01-25 12:59:29 -0500

    axix names added and sizes changed

--- mtcars/mtcars_project.R
+++ mtcars/mtcars_project.R
@@ -8,6 +8,11 @@ describe(mtcars)
 
 ggplot(mtcars, aes(cyl, mpg)) +
   geom_smooth(color = 'red', fill = "#78086C") +
-  theme_light() 
+  theme_light() +
+  theme(text = element_text(size = 12),
+        axis.title.x = element_text(color="black", size=12, face="bold"),
+        axis.title.y = element_text(color="black", size=12, face="bold")) +
+  xlab("Number of cylinders") +
+  ylab("Miles per gallon") 
   
 
