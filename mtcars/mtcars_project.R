ef0d1540354eb09be0ace2f32213a4450061ca44
saeedf92 <farajzadeh92@gmail.com>
2023-01-25 13:05:25 -0500

    Title added to the plot

--- mtcars/mtcars_project.R
+++ mtcars/mtcars_project.R
@@ -9,10 +9,11 @@ describe(mtcars)
 ggplot(mtcars, aes(cyl, mpg)) +
   geom_smooth(color = 'red', fill = "#78086C") +
   theme_light() +
-  theme(text = element_text(size = 12),
+  theme(text = element_text(size = 12), plot.title = element_text(size=14, face="bold"),
         axis.title.x = element_text(color="black", size=12, face="bold"),
         axis.title.y = element_text(color="black", size=12, face="bold")) +
   xlab("Number of cylinders") +
-  ylab("Miles per gallon") 
+  ylab("Miles per gallon") +
+  ggtitle("Association of number of cylinders and miles per gallon")
   
 
