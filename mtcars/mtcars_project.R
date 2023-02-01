cc2da7f9a3b4dea66b128b616603e07f2fe8e381
saeedf92 <farajzadeh92@gmail.com>
2023-02-01 11:27:37 -0500

    changed to first version

--- mtcars/mtcars_project.R
+++ mtcars/mtcars_project.R
@@ -1,19 +1,10 @@
 library(tidyverse)
+
 library(ggplot2)
-library(psych)
 
-
-describe(mtcars)
-
+mtcars
 
 ggplot(mtcars, aes(cyl, mpg)) +
-  geom_smooth(color = 'red', fill = "#78086C") +
-  theme_light() +
-  theme(text = element_text(size = 12), plot.title = element_text(size=14, face="bold"),
-        axis.title.x = element_text(color="black", size=12, face="bold"),
-        axis.title.y = element_text(color="black", size=12, face="bold")) +
-  xlab("Number of cylinders") +
-  ylab("Miles per gallon") +
-  ggtitle("Association of number of cylinders and miles per gallon")
-  
+  geom_smooth(color = 'red')
+
 
